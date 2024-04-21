9abeae03faad0d357d0e13f24b812151c7cc6bdc
Nicol Gustin <nicol930519@hotmail.com>
2024-04-21 00:26:46 -0500

    .

--- Supermarket-mvp3/Views/MainView.Designer.cs
+++ Supermarket-mvp3/Views/MainView.Designer.cs
@@ -30,9 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainView));
             panel1 = new Panel();
+            BtnExit = new Button();
             BtnPayMode = new Button();
             pictureBox1 = new PictureBox();
-            BtnExit = new Button();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
@@ -44,17 +44,31 @@
             panel1.Controls.Add(pictureBox1);
             panel1.Dock = DockStyle.Left;
             panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(3, 2, 3, 2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(250, 450);
+            panel1.Size = new Size(219, 338);
             panel1.TabIndex = 0;
+            // 
+            // BtnExit
+            // 
+            BtnExit.BackgroundImage = (Image)resources.GetObject("BtnExit.BackgroundImage");
+            BtnExit.BackgroundImageLayout = ImageLayout.Zoom;
+            BtnExit.Dock = DockStyle.Bottom;
+            BtnExit.Location = new Point(0, 273);
+            BtnExit.Margin = new Padding(3, 2, 3, 2);
+            BtnExit.Name = "BtnExit";
+            BtnExit.Size = new Size(219, 65);
+            BtnExit.TabIndex = 2;
+            BtnExit.UseVisualStyleBackColor = true;
             // 
             // BtnPayMode
             // 
             BtnPayMode.BackgroundImage = (Image)resources.GetObject("BtnPayMode.BackgroundImage");
             BtnPayMode.BackgroundImageLayout = ImageLayout.Zoom;
-            BtnPayMode.Location = new Point(0, 92);
+            BtnPayMode.Location = new Point(0, 69);
+            BtnPayMode.Margin = new Padding(3, 2, 3, 2);
             BtnPayMode.Name = "BtnPayMode";
-            BtnPayMode.Size = new Size(250, 100);
+            BtnPayMode.Size = new Size(219, 75);
             BtnPayMode.TabIndex = 1;
             BtnPayMode.UseVisualStyleBackColor = true;
             // 
@@ -62,30 +76,22 @@
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
             pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(250, 90);
+            pictureBox1.Size = new Size(219, 68);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
-            // 
-            // BtnExit
-            // 
-            BtnExit.BackgroundImage = (Image)resources.GetObject("BtnExit.BackgroundImage");
-            BtnExit.BackgroundImageLayout = ImageLayout.Zoom;
-            BtnExit.Dock = DockStyle.Bottom;
-            BtnExit.Location = new Point(0, 363);
-            BtnExit.Name = "BtnExit";
-            BtnExit.Size = new Size(250, 87);
-            BtnExit.TabIndex = 2;
-            BtnExit.UseVisualStyleBackColor = true;
+            pictureBox1.Click += pictureBox1_Click;
             // 
             // MainView
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
             Controls.Add(panel1);
             IsMdiContainer = true;
+            Margin = new Padding(3, 2, 3, 2);
             Name = "MainView";
             Text = "MainView";
             WindowState = FormWindowState.Maximized;
--- Supermarket-mvp3/Views/MainView.cs
+++ Supermarket-mvp3/Views/MainView.cs
@@ -29,5 +29,10 @@ namespace Supermarket_mvp3.Views
         {
 
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
