29a72f4afa82a21e8d5d8ac3b901fcb2c05a168f
Nicol Gustin <nicol930519@hotmail.com>
2024-04-20 23:55:43 -0500

    IProvidersRepository

--- Supermarket-mvp3/obj/Debug/net8.0-windows/Supermarket-mvp3.AssemblyInfo.cs
+++ Supermarket-mvp3/obj/Debug/net8.0-windows/Supermarket-mvp3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dab60deee5e7515af404cbb90baa2234871bf0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37a703c1537e695a0dc3ea10bf0d5b7f4c632b60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
